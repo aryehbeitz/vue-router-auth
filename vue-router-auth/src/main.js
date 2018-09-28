01ff510c606594b351c38f8746a36d0a6a060e56
Aryeh Beitz <aryehbeitz@gmail.com>
2018-09-28 15:41:58 +0300

    initial commit

---
+++ vue-router-auth/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+
+Vue.prototype.$http = Axios;
+
+Vue.config.productionTip = false
+
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+})
